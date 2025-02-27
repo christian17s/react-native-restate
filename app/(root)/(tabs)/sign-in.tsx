0f12838e214d3fc2e05925d92b6ea31abd4d136e
christian <christian17silveira@gmail.com>
2025-02-27 14:27:00 -0300

    fonts, image, loading configs

--- app/(root)/(tabs)/sign-in.tsx
+++ app/(root)/(tabs)/sign-in.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const signIn = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>signIn</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
