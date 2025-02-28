87e49808f2fc1954396c13507b4e956165962b8d
christian <christian17silveira@gmail.com>
2025-02-27 23:07:29 -0300

    getUser function

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -63,3 +63,22 @@ export async function logout() {
         return false;
     }
 }
+
+export async function getUser() {
+    try {
+        const response = await account.get();
+
+        if(response.$id) {
+            const userAvatar = avatar.getInitials(response.name)
+
+            return {
+                ...response,
+                avatar: userAvatar.toString(),
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
