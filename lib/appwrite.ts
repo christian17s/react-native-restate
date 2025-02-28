513971482e4495f8bcaab4ef095dd4d3677f180d
christian <christian17silveira@gmail.com>
2025-02-28 00:02:56 -0300

    Library changes

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,84 +1,86 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
-import * as Linking from 'expo-linking';
+import { 
+    Account, 
+    Avatars, 
+    Client, 
+    OAuthProvider, 
+} from "react-native-appwrite";
+import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-    platform: 'com.jsm.restate',
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+  platform: "com.jsm.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
 
 export const client = new Client();
 
 client
-    .setEndpoint(config.endpoint!)
-    .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 
-
 export async function login() {
-    try {
-        const redirectUri = Linking.createURL('/');
+  try {
+    const redirectUri = Linking.createURL("/");
 
-        const response = await account.createOAuth2Token(
-            OAuthProvider.Google, 
-            redirectUri
-        );
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
 
-        if(!response) throw new Error('Failed to login');
+    if (!response) throw new Error("Failed to login");
 
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUri
-        )
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
 
-        if(browserResult.type !== 'success') throw new Error('Failed to login');
+    if (browserResult.type !== "success")
+      throw new Error("Create OAuth2 token failed");
 
-        const url = new URL(browserResult.url);
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+    if (!secret || !userId) throw new Error("Failed to login");
 
-        const secret = url.searchParams.get('secret')?.toString();
-        const userId = url.searchParams.get('userId')?.toString();
+    const session = await account.createSession(userId, secret);
+    if (!session) throw new Error("Failed to create session");
 
-        if(!secret || !userId) throw new Error('Failed to login');
-
-        const session = await account.createSession(userId, secret);
-
-        if(!session) throw new Error('Failed to login');
-
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export async function logout() {
-    try {
-        await account.deleteSession('current');
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export async function getUser() {
-    try {
-        const response = await account.get();
+  try {
+    const response = await account.get();
 
-        if(response.$id) {
-            const userAvatar = avatar.getInitials(response.name)
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
 
-            return {
-                ...response,
-                avatar: userAvatar.toString(),
-            }
-        }
-
-    } catch (error) {
-        console.log(error);
-        return null;
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
     }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
